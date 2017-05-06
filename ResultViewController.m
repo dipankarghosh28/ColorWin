9a71fd24a5f138d6e01f9bc61c3b2149a8dc0cfb
Dipankar Ghosh <dipankarghosh@Dipankars-MacBook-Air.local>
2017-05-06 14:58:44 -0400

    ResultController::HighestScore save, scorerecord update

--- ResultViewController.m
+++ ResultViewController.m
@@ -5,3 +5,47 @@
 //  Created by Dipankar Ghosh on 4/26/17.
 //  Copyright © 2017 Dipankar Ghosh. All rights reserved.
 //
+#import "ResultViewController.h"
+#import "ColorUtils.h"
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *scoreRecordText;
+@property (weak, nonatomic) IBOutlet UILabel *scoreText;
+
+@end
+
+@implementation ResultViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    NSInteger highestScore = [ColorUtils getHighestScore];
+    if (highestScore < self.score) {
+        highestScore = self.score;
+        [ColorUtils saveHighestScore:self.score];
+    }
+    
+    self.scoreRecordText.text = [NSString stringWithFormat:@"%d", (int)highestScore];
+    self.scoreText.text = [NSString stringWithFormat:@"%d", (int)self.score];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)tryAgain:(id)sender {
+    [self.navigationController popViewControllerAnimated:TRUE];
+}
+
+@end
