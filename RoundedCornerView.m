46c71d984ba100249327ed8aee3e658c2dd2e2f5
Dipankar Ghosh <dipankarghosh@Dipankars-MacBook-Air.local>
2017-05-06 15:05:34 -0400

    Update on the rounded Corner

--- RoundedCornerView.m
+++ RoundedCornerView.m
@@ -5,3 +5,35 @@
 //  Created by Dipankar Ghosh on 4/26/17.
 //  Copyright © 2017 Dipankar Ghosh. All rights reserved.
 //
+#import "RoundedCornerView.h"
+
+@implementation RoundedCornerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+- (id)initWithCoder:(NSCoder *)aDecoder;
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)commonInit
+{
+    CALayer *layer = self.layer;
+    layer.cornerRadius  = 10.0f;
+    layer.masksToBounds = YES;
+}
+
+
+@end
