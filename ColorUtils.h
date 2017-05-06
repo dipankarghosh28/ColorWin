2b892361ef60aee58c43e84882d6c60e2dfca414
Dipankar Ghosh <dipankarghosh@Dipankars-MacBook-Air.local>
2017-05-06 13:17:28 -0400

    ColorUtils

--- ColorUtils.h
+++ ColorUtils.h
@@ -5,3 +5,23 @@
 //  Created by Dipankar Ghosh on 4/26/17.
 //  Copyright © 2017 Dipankar Ghosh. All rights reserved.
 //
+
+#import <UIKit/UIKit.h>
+@interface ColorUtils : NSObject
+
++ (UIColor *)redColor;
++ (UIColor *)blueColor;
++ (UIColor *)greenColor;
++ (UIColor *)yellowColor;
+
+
++ (NSArray *) validColorNames;
++ (NSArray *) validColors;
++ (NSString *)randomColorText;
++ (UIColor *)randomColor:(NSArray *)colors;
++ (NSUInteger)randomIndex:(NSUInteger)colorCount;
+
+
++ (void)saveHighestScore:(NSInteger)score;
++ (NSInteger)getHighestScore;
+@end
