1c769158d6ad801a535d527b84f791e7c2239748
Dipankar Ghosh <dipankarghosh@Dipankars-MacBook-Air.local>
2017-05-06 14:24:48 -0400

    HelpViewControllerAdding images to the file

--- HelpViewController.m
+++ HelpViewController.m
@@ -5,3 +5,41 @@
 //  Created by Dipankar Ghosh on 4/26/17.
 //  Copyright © 2017 Dipankar Ghosh. All rights reserved.
 //
+#import "HelpViewController.h"
+
+@interface HelpViewController ()
+@property (weak, nonatomic) IBOutlet UIView *imageView;
+
+@end
+
+@implementation HelpViewController
+
+- (void) updateImage
+{
+    for (UIView *view in self.imageView.subviews) {
+        [view removeFromSuperview];
+    }
+    
+    UIImageView *imageSubView = [[UIImageView alloc] initWithFrame:self.imageView.bounds];
+    imageSubView.image = [UIImage imageNamed:@"screenshot2"];
+    [self.imageView addSubview:imageSubView];
+}
+
+
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    [self updateImage];
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self updateImage];
+    
+    
+}
+- (IBAction)backGame:(id)sender {
+    [self.navigationController popViewControllerAnimated:TRUE];
+}
+
+@end
