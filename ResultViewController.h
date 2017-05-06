a33c631db95cd344ad1b1c220a885a39b80dabad
Dipankar Ghosh <dipankarghosh@Dipankars-MacBook-Air.local>
2017-05-06 14:42:49 -0400

    Started working on the Result Controller then shall connect it via segue property to color beat controller

--- ResultViewController.h
+++ ResultViewController.h
@@ -5,3 +5,10 @@
 //  Created by Dipankar Ghosh on 4/26/17.
 //  Copyright © 2017 Dipankar Ghosh. All rights reserved.
 //
+
+#import <UIKit/UIKit.h>
+
+@interface ResultViewController : UIViewController
+@property (nonatomic) NSInteger score;
+
+@end
