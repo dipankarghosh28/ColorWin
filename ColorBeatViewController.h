5bcdde3cf34e1f7be8f0dda4536575c21c192d88
Dipankar Ghosh <dipankarghosh@Dipankars-MacBook-Air.local>
2017-05-06 14:26:58 -0400

    Now working on the ColorViewController, updated the ColorViewController.h

--- ColorBeatViewController.h
+++ ColorBeatViewController.h
@@ -5,3 +5,9 @@
 //  Created by Dipankar Ghosh on 4/26/17.
 //  Copyright © 2017 Dipankar Ghosh. All rights reserved.
 //
+
+#import <UIKit/UIKit.h>
+
+@interface ColorBeatViewController : UIViewController
+
+@end
